b4cc2911eb6a0acbc71dcf09f08fbe5778900b7f
Ionut_B <barborionut15@gmail.com>
2023-09-18 14:51:11 +0300

    well the screen resized option still broken and needs some fixing

---
+++ includes/weeknumber.cpp
@@ -0,0 +1,23 @@
+#include <ctime>
+#include <math.h>
+#define ConvertNr 604800
+
+int getWeekNumber()
+{
+	double seconds;
+	
+	time_t CurrentDate;
+	time(&CurrentDate);
+
+	struct tm StartDate {0};
+	
+	StartDate.tm_hour = 0;   StartDate.tm_min = 0; StartDate.tm_sec = 0;
+  	StartDate.tm_year = 2023-1900; StartDate.tm_mon = 9; StartDate.tm_mday = 2;
+	
+	//get the difference in sec and make it positive 
+	seconds = fabs(difftime(CurrentDate, mktime(&StartDate)));
+
+	double weekNumber = seconds/ConvertNr;
+	int weekNumberRounded = ceil(weekNumber);
+	return weekNumberRounded;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
+#include <string>
 #include <locale.h>
 #include <ncurses.h>
-#include <string>
+#include <signal.h>
+#include "../includes/weeknumber.hpp"
 
 int numrows, numcols , yMax, xMax;
 char exitKey;
 
-const char *timeTable[5][8] = {
-	{"header1", "header2", "header3"},
-	{"header1", "header2", "header3"},
-	{"header1", "header2", "header3"},
-	{"data1", "data2", "data3"},
-	{"data4", "data5", "data6"},
+std::string timeTable[7][6] = {
+	{"Program", "Monday", "Tuesday","Wednesday", "Thursday", "Friday"},
+	{" 8:00 - 10:00", "header2", "header3","header1", "header2", "header3"},
+	{"10:00 - 12:00", "header2", "header3","header1", "header2", "header3"},
+	{"12:00 - 14:00", "header", "header3","header1", "header2", "header3"},
+	{"14:00 - 16:00", "header2", "header3","header1", "header2", "hwwwwwwwwweader3"},
+	{"16:00 - 18:00", "header2", "header3","header1", "header2", "header3"},
+	{"18:00 - 20:00", "header2", "header3","header1", "headwiwwjwwjwer2", "header3"},
 };
 
+// Broken need fixing
+void handleResize(int signal)
+{
+	endwin(); 
+	refresh();
+	initscr();
+	refresh(); 
+}
 void print_center(WINDOW *win,int start_row , std::string text)
 {
 	int center_col = win ->_maxx/2;
@@ -36,18 +48,24 @@ void drawLogo(WINDOW *win)
 	╚══════╝   ╚═╝    ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝   ╚═╝       ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝	)";
 	print_center(stdscr,yMax/7,"Student Dashboard");
 }
+void getCurrentDate()
+{
+	return;	
+}
 void writeInfoText(WINDOW *win)
 {
 	print_center(stdscr,yMax/5,"Right now u have " "" "at");
 }
 
-void drawtable(WINDOW *win, const char* timeTable[5][8], int numrows, int numcols) 
+void drawtable(WINDOW *win,std::string timeTable[7][6], int numrows, int numcols) 
 {
 	for (int i = 0; i < numrows; i++) 
 	{
 		for (int j = 0; j < numcols; j++) 
 		{
-			mvwprintw(win,i+15, j * 20, "%-20s", timeTable[i][j]);
+			//hardcoded ik its bad but it works
+			int centeredCol = xMax/2 - 106/2; 
+			mvwprintw(win,i+xMax/15, j * 20 + centeredCol, "%-15s", timeTable[i][j].c_str());
 		};
 	};
 };	
@@ -59,26 +77,33 @@ int main (int argc, char *argv[])
 	setlocale(LC_ALL, "");
 	raw();
 	cbreak();
+	nodelay(stdscr, TRUE);
 	curs_set(0);
 	keypad(stdscr, TRUE);
-
+	nodelay(stdscr, TRUE);
+	
 	getmaxyx(stdscr,yMax ,xMax);
 
 	WINDOW *win = newwin(yMax-2, xMax-2 , 1, 1);
+	//testing
+	//WINDOW *secondary = newwin(yMax-2, xMax/2 , yMax/2, xMax/2);
 	refresh();	
 
 	drawLogo(win);
 	writeInfoText(win);
-	drawtable(win, timeTable ,5 ,8 );
-
+	drawtable(win, timeTable ,7 ,6 );
+	
 	box(win, 0, 0);
 	wrefresh(win);
-	
+
 	//to be modified with a switch case 
 	while ((exitKey = getch()) != 'q') {
 
 	}
-
+	
+	//probrably spould be check out
+	signal(SIGWINCH, handleResize);
+	
 	endwin();
 	return 0;
 }
